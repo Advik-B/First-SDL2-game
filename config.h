904b760e63e628dca206f4411827d992f01acfe6
Advik <advik.b@gmail.com>
2022-10-05 20:41:22 +0530

    Add project files.

---
+++ config.h
@@ -0,0 +1,9 @@
+#include <vector>
+
+namespace config
+
+{
+	using std::vector;
+	vector<int> window_size{ 600, 400 };
+	vector<int> background{ 0, 255, 0, 255 };
+}
---
+++ main.cpp
@@ -0,0 +1,33 @@
+#define SDL_MAIN_HANDLED
+#include "SDL.h"
+#include <vector>
+#include "config.h"
+
+using namespace std;
+using config::background;
+
+int main(int argc, char *argv)
+{	
+	SDL_SetMainReady();
+	SDL_Init(SDL_INIT_EVERYTHING);
+
+	SDL_Window *window
+		= SDL_CreateWindow(
+			"Game title",
+			SDL_WINDOWPOS_CENTERED,
+			SDL_WINDOWPOS_CENTERED,
+			config::window_size[0], config::window_size[1],
+			SDL_WINDOW_SHOWN
+		);
+
+	SDL_Renderer* renderer = SDL_CreateRenderer(window, -1, 0);
+	SDL_SetRenderDrawColor(
+		renderer,
+		background[0],
+		background[1],
+		background[2], 
+		background[3]
+	);
+	
+	return 0;
+}
